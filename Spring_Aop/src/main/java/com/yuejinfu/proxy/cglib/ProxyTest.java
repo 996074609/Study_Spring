1328465f16cbf385382044eff5c90b20cb150f7e
Your Name <15594143534@163.com>
2019-12-26 17:51:14 +0800

    Spring_Aop

---
+++ Spring_Aop/src/main/java/com/yuejinfu/proxy/cglib/ProxyTest.java
@@ -0,0 +1,14 @@
+package com.yuejinfu.proxy.cglib;
+
+public class ProxyTest {
+
+
+    final Target target = new Target();
+
+    final Advice advice = new Advice();
+
+    //返回值就是动态生成的代理对象
+
+    
+
+}
---
+++ Spring_Aop/src/main/java/com/yuejinfu/proxy/cglib/Target.java
@@ -0,0 +1,13 @@
+package com.yuejinfu.proxy.cglib;
+
+
+/**
+ * 目标对象
+ */
+public class Target {
+
+    public void save(){
+        System.out.println("running....");
+    }
+
+}
